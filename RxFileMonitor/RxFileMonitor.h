9342ed5e82112bb5169d4556d24668d5fb0e92e9
Christian Tietze <christian.tietze@gmail.com>
2016-11-08 08:12:43 +0100

    Initial Commit

---
+++ RxFileMonitor/RxFileMonitor.h
@@ -0,0 +1,19 @@
+//
+//  RxFileMonitor.h
+//  RxFileMonitor
+//
+//  Created by Christian Tietze on 08/11/16.
+//  Copyright © 2016 CleanCocoa. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RxFileMonitor.
+FOUNDATION_EXPORT double RxFileMonitorVersionNumber;
+
+//! Project version string for RxFileMonitor.
+FOUNDATION_EXPORT const unsigned char RxFileMonitorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxFileMonitor/PublicHeader.h>
+
+

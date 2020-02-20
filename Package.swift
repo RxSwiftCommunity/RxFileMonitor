76780e8f2bf4acd68fb3371277284e3dc50ff8db
Marius Landwehr <marius@deepl.com>
2020-02-20 07:56:06 +0100

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "RxFileMonitor",
+    platforms: [
+        .macOS(.v10_11),
+    ],
+    products: [
+        .library(name: "RxFileMonitor", targets: ["RxFileMonitor"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift", from: "5.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "RxFileMonitor",
+            dependencies: ["RxSwift"],
+            path: "RxFileMonitor"
+        )
+    ]
+)

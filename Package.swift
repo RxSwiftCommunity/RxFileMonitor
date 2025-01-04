6b9bc2379700758c33c145e74980ff4f2825ab7a
Christian Tietze <me@christiantietze.de>
2025-01-04 12:14:44 +0100

    bump RxSwift and Swift version

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
-// swift-tools-version:5.0
+// swift-tools-version: 5.7
 import PackageDescription
 
 let package = Package(
     name: "RxFileMonitor",
     platforms: [
-        .macOS(.v10_11),
+        .macOS(.v10_13),
     ],
     products: [
         .library(name: "RxFileMonitor", targets: ["RxFileMonitor"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift", from: "5.0.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift", from: "6.8.0"),
     ],
     targets: [
         .target(
